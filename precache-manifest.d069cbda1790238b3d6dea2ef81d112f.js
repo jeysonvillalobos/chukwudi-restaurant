265d4f34df83f5869eefc069fd075fb309e292f6
Jeyson Villalobos <jeyson3213@hotmail.com>
2020-08-04 12:28:36 -0500

    Updates

---
+++ precache-manifest.d069cbda1790238b3d6dea2ef81d112f.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1f6b6357cf04e2a264a42b25875dbd6",
+    "url": "/chukwudi-restaurant/index.html"
+  },
+  {
+    "revision": "9fa2bbe88333a2c36147",
+    "url": "/chukwudi-restaurant/static/css/2.9c8c8187.chunk.css"
+  },
+  {
+    "revision": "e011b5bf64515281cf42",
+    "url": "/chukwudi-restaurant/static/css/main.2dfe18f0.chunk.css"
+  },
+  {
+    "revision": "9fa2bbe88333a2c36147",
+    "url": "/chukwudi-restaurant/static/js/2.42bc7fa7.chunk.js"
+  },
+  {
+    "revision": "7bfaa32b315fe466d84ab2ae49052d17",
+    "url": "/chukwudi-restaurant/static/js/2.42bc7fa7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e011b5bf64515281cf42",
+    "url": "/chukwudi-restaurant/static/js/main.1da0d5d5.chunk.js"
+  },
+  {
+    "revision": "8a4558a95f260fe2c748",
+    "url": "/chukwudi-restaurant/static/js/runtime-main.7240296c.js"
+  },
+  {
+    "revision": "96c00f56316098f546d1cf99e7b15cb5",
+    "url": "/chukwudi-restaurant/static/media/1046784.96c00f56.svg"
+  },
+  {
+    "revision": "279196e37844ff702af163035a67c609",
+    "url": "/chukwudi-restaurant/static/media/109617.279196e3.svg"
+  },
+  {
+    "revision": "1ee4c3ee10a89d4ebe3782b2ff0d9eea",
+    "url": "/chukwudi-restaurant/static/media/118740.1ee4c3ee.svg"
+  },
+  {
+    "revision": "63fff94824279a5a1b3309bcfa0229d2",
+    "url": "/chukwudi-restaurant/static/media/126474.63fff948.svg"
+  },
+  {
+    "revision": "88688cf0265e297262d0cf7470c1609c",
+    "url": "/chukwudi-restaurant/static/media/149220.88688cf0.svg"
+  },
+  {
+    "revision": "4e02a388adbabe44ed3e306ea9e14d14",
+    "url": "/chukwudi-restaurant/static/media/car.4e02a388.svg"
+  },
+  {
+    "revision": "9dc6cbb136d7e602d828777da3794e33",
+    "url": "/chukwudi-restaurant/static/media/face.9dc6cbb1.png"
+  },
+  {
+    "revision": "cdead91be912190bf9457441f88d3002",
+    "url": "/chukwudi-restaurant/static/media/headerimage.cdead91b.png"
+  },
+  {
+    "revision": "ffc7b8d9a46fc9dc099d1bdf318e2f54",
+    "url": "/chukwudi-restaurant/static/media/more.ffc7b8d9.png"
+  },
+  {
+    "revision": "86d434a7f294490a4b2b3a01c66e6697",
+    "url": "/chukwudi-restaurant/static/media/whiteClock.86d434a7.svg"
+  },
+  {
+    "revision": "34e7d6a15529cd3acd0c4c2fb47cf37b",
+    "url": "/chukwudi-restaurant/static/media/whiteShortRightArrow.34e7d6a1.svg"
+  },
+  {
+    "revision": "6ddbc5536cd58728a1ea2db28fa8bbaf",
+    "url": "/chukwudi-restaurant/static/media/yellowClock.6ddbc553.svg"
+  }
+]);
